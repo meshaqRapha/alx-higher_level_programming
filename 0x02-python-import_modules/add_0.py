690f463c9176d1a2e9c9652caa5fc58d330e8d3f
meshaqRapha <meshaqRapha@gmail.com>
2022-09-22 15:09:19 -0700

    Task 0, add_0.py

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
-
 def add(a, b):
-    return(a + b)
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
